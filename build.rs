ab3b03dbdf2c89dc1058f86ceb731a148427a2b6
Dominic Fox <dominicjfox2@gmail.com>
2026-08-31 18:56:59 +0100

    fixed some old file paths

--- build.rs
+++ build.rs
@@ -2,11 +2,11 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    // 1. Point directly to your existing compiled C directory
+    //Point directly to your existing compiled C directory
     let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    let src_dir = PathBuf::from(manifest_dir).join("../Schemes/the-sqisign/build/src");
+    let src_dir = PathBuf::from(manifest_dir).join("variant/sqisign-v2/build/src");
 
-    // 2. Add the exact search path for every mathematical submodule
+    // Add the search path for every mathematical submodule
     println!("cargo:rustc-link-search=native={}", src_dir.display());
     println!("cargo:rustc-link-search=native={}/signature/ref/lvl1", src_dir.display());
     println!("cargo:rustc-link-search=native={}/verification/ref/lvl1", src_dir.display());
@@ -19,10 +19,10 @@ fn main() {
     println!("cargo:rustc-link-search=native={}/mp/ref/generic", src_dir.display());
     println!("cargo:rustc-link-search=native={}/common/generic", src_dir.display());
 
-    // 1. Tell Cargo to look in the Apple Silicon Homebrew directory
+    // Tell Cargo to look in the Apple Silicon Homebrew directory
     println!("cargo:rustc-link-search=native=/opt/homebrew/lib");
 
-    // 3. Link every required component in reverse dependency order
+    // Link every required component
     println!("cargo:rustc-link-lib=static=sqisign_lvl1_nistapi");
     println!("cargo:rustc-link-lib=static=sqisign_lvl1");
     println!("cargo:rustc-link-lib=static=sqisign_signature_lvl1");
@@ -36,6 +36,6 @@ fn main() {
     println!("cargo:rustc-link-lib=static=sqisign_mp_generic");
     println!("cargo:rustc-link-lib=static=sqisign_common_sys");
 
-    // 2. Link the GMP library dynamically
+    // Link the GMP library dynamically
     println!("cargo:rustc-link-lib=dylib=gmp");
 }
--- src/main.rs
+++ src/main.rs
@@ -12,75 +12,75 @@ fn main() {
     let schemes = vec![
         SchemeConfig { 
             name: "SQISign NIST Round 2 Submission", 
-            path: "../Schemes/the-sqisign/build/apps/benchmark_binary", 
+            path: "variant/sqisign-v2/build/apps/benchmark_binary", 
             language: "C",
-            levels: vec![1,3,5],
+            levels: vec![1, 3, 5],
             output_csv_fixed_key: "results/sqisign_fixed_key_benchmark.csv",
             output_csv_fixed_msg: "results/sqisign_fixed_msg_benchmark.csv",
             output_csv_keygen: "results/sqisign_keygen_benchmark.csv",
-            stack_path: "../Schemes/the-sqisign/build/apps/stack_probe",
+            stack_path: "variant/sqisign-v2/build/apps/stack_probe",
             output_csv_stack: "results/sqisign_stack_benchmark.csv",
-            is_enabled: false
+            is_enabled: true
         },
         SchemeConfig { 
             name: "SQISign2D-West", 
-            path: "../Schemes/sqisign2d-west-ac24/build/test/test_sqisigndim2_modified", 
+            path: "variant/sqisign-2dwest/build/test/test_sqisigndim2_modified", 
             language: "C",
             levels: vec![1, 3, 5],
             output_csv_fixed_key: "results/sqisign2dwest_fixed_key_benchmark.csv",
             output_csv_fixed_msg: "results/sqisign2dwest_fixed_msg_benchmark.csv",
             output_csv_keygen: "results/sqisign2dwest_keygen_benchmark.csv",
-            stack_path: "../Schemes/sqisign2d-west-ac24/build/test/stack_probe_2dwest",
+            stack_path: "variant/sqisign-2dwest/build/test/stack_probe_2dwest",
             output_csv_stack: "results/sqisign2dwest_stack_benchmark.csv",
-            is_enabled: false
+            is_enabled: true
         },
         SchemeConfig { 
             name: "SQISign2D-West Heuristic", 
-            path: "../Schemes/sqisign2d-west-ac24/build/test/test_sqisigndim2_heuristic_modified", 
+            path: "variant/sqisign-2dwest/build/test/test_sqisigndim2_heuristic_modified", 
             language: "C",
-            levels: vec![1,3,5],
+            levels: vec![1, 3, 5],
             output_csv_fixed_key: "results/sqisign2dwestheuristic_fixed_key_benchmark.csv",
             output_csv_fixed_msg: "results/sqisign2dwestheuristic_fixed_msg_benchmark.csv",
             output_csv_keygen: "results/sqisign2dwestheuristic_keygen_benchmark.csv",
-            stack_path: "../Schemes/sqisign2d-west-ac24/build/test/stack_probe_2dwestheuristic",
+            stack_path: "variant/sqisign-2dwest/build/test/stack_probe_2dwestheuristic",
             output_csv_stack: "results/sqisign2dwestheuristic_stack_benchmark.csv",
             is_enabled: true
         },
         SchemeConfig { 
             name: "SQISignHD", 
-            path: "../Schemes/SQISignHD-lib/Signature/build/test/sqisign_test_sqisignhd_modified",
+            path: "variant/sqisignhd/Signature/build/test/sqisign_test_sqisignhd_modified",
             language: "C",
             levels: vec![1, 3, 5],
             output_csv_fixed_key: "results/sqisignhd_fixed_key_benchmark.csv",
             output_csv_fixed_msg: "results/sqisignhd_fixed_msg_benchmark.csv",
             output_csv_keygen: "results/sqisignhd_keygen_benchmark.csv",
-            stack_path: "../Schemes/SQISignHD-lib/Signature/build/test/stack_probe_hd",
+            stack_path: "variant/sqisignhd/Signature/build/test/stack_probe_hd",
             output_csv_stack: "results/sqisignhd_stack_benchmark.csv",
-            is_enabled: false
+            is_enabled: true
         },
         SchemeConfig { 
             name: "SQISign NIST Round 1 Submission", 
-            path: "../Schemes/the-sqisign-v1/build/apps/benchmark_binary", 
+            path: "variant/sqisign-v1/build/apps/benchmark_binary", 
             language: "C",
-            levels: vec![1,3,5],
+            levels: vec![1, 3, 5],
             output_csv_fixed_key: "results/sqisignv1_fixed_key_benchmark.csv",
             output_csv_fixed_msg: "results/sqisignv1_fixed_msg_benchmark.csv",
             output_csv_keygen: "results/sqisignv1_keygen_benchmark.csv",
-            stack_path: "../Schemes/the-sqisign-v1/build/apps/stack_probe",
+            stack_path: "variant/sqisign-v1/build/apps/stack_probe",
             output_csv_stack: "results/sqisignv1_stack_benchmark.csv",
-            is_enabled: false
+            is_enabled: true
         },
         SchemeConfig { 
             name: "SQISign2D-East", 
-            path: "../Schemes/SQIsign2D-East.jl/benchmark_2deast.jl", //Just change number
+            path: "variant/sqisign-2deast/benchmark_2deast.jl", 
             language: "Julia",
-            levels: vec![5],
+            levels: vec![1, 3, 5],
             output_csv_fixed_key: "results/sqisign2deast_fixed_key_benchmark.csv",
             output_csv_fixed_msg: "results/sqisign2deast_fixed_msg_benchmark.csv",
             output_csv_keygen: "results/sqisign2deast_keygen_benchmark.csv",
             stack_path: "",
             output_csv_stack: "results/sqisign2deast_stack_benchmark.csv",
-            is_enabled: false
+            is_enabled: true
         },
         
     ];
@@ -90,29 +90,29 @@ fn main() {
     // ============================================================================
 
     // GLOBAL PARAMS
-    let total_batches = 10;
+    let total_batches = 5;
     let warmup_iterations = 15;
     let thermal_secs = 15;
 
     // MODE 0: KEYGEN PROFILING PARAMS (Lots of keys, no iteration)
     let run_keygen_profiling = true;  
-    let keys_for_profiling = 1000;
+    let keys_for_profiling = 100;
 
     // MODE 1: FIXED KEY PARAMS (1 Key, N Messages per batch)
     let run_fixed_key_suite = true; 
-    let num_rand_messages = 1000;
+    let num_rand_messages = 100;
     
     // MODE 2: FIXED MSG PARAMS (N Keys, 1 Message per batch)
     let run_fixed_msg_suite = true; 
-    let num_keys = 1000;
+    let num_keys = 100;
 
-    // MODE 3: STACK HIGH-WATER PARAMS
-    let run_stack_suite = false;
+    // MODE 3: STACK PROFILING PARAMS
+    let run_stack_suite = true;
     let stack_iterations = 50;
-    let stack_batches = 2;
+    let stack_batches = 1;
     
 
-    println!("[*] Starting Master Overnight Benchmark Suite");
+    println!("[*] Starting Benchmark Suite");
 
     for scheme in schemes.into_iter().filter(|s| s.is_enabled) {
 
@@ -171,7 +171,7 @@ fn main() {
             }
 
             // ---------------------------------------------------------
-            // MODE 3: Stack High-Water Profiling
+            // MODE 3: Stack Profiling
             // ---------------------------------------------------------
             if run_stack_suite {
                 if scheme.stack_path.is_empty() {
